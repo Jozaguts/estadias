73b402197d7531c6ddd143469e8f717e5ed7a226
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-03-19 10:10:07 -0600

    add our services

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,39 +23,7 @@
 //
 // })(jQuery);
 //
-// $(document).ready(function(){
-//     var owl = $('.owl-carousel');
-//     owl.owlCarousel({
-//         items:3,
-//         loop:true,
-//         margin:10,
-//         autoplay:true,
-//         autoplayTimeout:3000,
-//         autoplayHoverPause:true,
-//         // responsiveClass:true,
-//         responsive:{
-//             0:{
-//                 items:1,
-//                 nav:true
-//             },
-//             600:{
-//                 items:1,
-//                 nav:false
-//             },
-//             1000:{
-//                 items:3,
-//                 nav:true,
-//                 loop:false
-//             }
-//         }
-//     });
-//     $('.play').on('click',function(){
-//         owl.trigger('play.owl.autoplay',[1000])
-//     })
-//     $('.stop').on('click',function(){
-//         owl.trigger('stop.owl.autoplay')
-//     })
-// });
+
 //
 // //galeria proveedores
 //
