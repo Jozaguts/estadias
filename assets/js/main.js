347c4d0152e4b72c3b24edf2362086aa317a4a51
Sagit Gutiérrez <jozaguts@hotmail.com>
2020-03-19 08:52:16 -0600

    add header footer and about zone refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,63 @@
-let toggleBtn = document.getElementById('toggleBtn')
-
-let toggleStatus = false;
-function toggleClass() {
-    let navBar = document.getElementById('navBar')
-    if(!toggleStatus) {
-        navBar.classList.remove('d-none');
-        toggleStatus = !toggleStatus;
-    }else {
-        navBar.classList.add('d-none');
-        toggleStatus = !toggleStatus;
-    }
-}
-toggleBtn.addEventListener('click',toggleClass);
-
-//carrusel
-(function($) {
-    "use strict";
-
-    // manual carousel controls
-    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
-    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
-    
-})(jQuery);
-
-//galeria proveedores
-
+// let toggleBtn = document.getElementById('toggleBtn')
+//
+// let toggleStatus = false;
+// function toggleClass() {
+//     let navBar = document.getElementById('navBar')
+//     if(!toggleStatus) {
+//         navBar.classList.remove('d-none');
+//         toggleStatus = !toggleStatus;
+//     }else {
+//         navBar.classList.add('d-none');
+//         toggleStatus = !toggleStatus;
+//     }
+// }
+// toggleBtn.addEventListener('click',toggleClass);
+//
+// //carrusel
+// (function($) {
+//     "use strict";
+//
+//     // manual carousel controls
+//     $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+//     $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
+//
+// })(jQuery);
+//
+// $(document).ready(function(){
+//     var owl = $('.owl-carousel');
+//     owl.owlCarousel({
+//         items:3,
+//         loop:true,
+//         margin:10,
+//         autoplay:true,
+//         autoplayTimeout:3000,
+//         autoplayHoverPause:true,
+//         // responsiveClass:true,
+//         responsive:{
+//             0:{
+//                 items:1,
+//                 nav:true
+//             },
+//             600:{
+//                 items:1,
+//                 nav:false
+//             },
+//             1000:{
+//                 items:3,
+//                 nav:true,
+//                 loop:false
+//             }
+//         }
+//     });
+//     $('.play').on('click',function(){
+//         owl.trigger('play.owl.autoplay',[1000])
+//     })
+//     $('.stop').on('click',function(){
+//         owl.trigger('stop.owl.autoplay')
+//     })
+// });
+//
+// //galeria proveedores
+//
+//
+//
